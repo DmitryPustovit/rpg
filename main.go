5e2757cc601be56dcd2133e6963589c5139e2e38
DmitryPustovit <dmitryp222@gmail.com>
2020-01-06 19:22:46 -0800

    first!

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":" + port, nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s! You rolled.... A Natural 1. Critial Fail.", r.URL.Path[1:])
+}
